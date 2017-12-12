f69a43db736f6875af85fa23f0a61a8b122789d0
lefty-son <dangen.effy@gmail.com>
2017-12-12 19:31:37 +0900

    proto

---
+++ Assets/Script/Graphic/ConveyorScroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorScroller : MonoBehaviour {
+
+    public float speed;
+    private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = new Vector3(-Time.time * speed, 0, 0);
+        rend.material.mainTextureOffset = offset;
+	}
+}
---
+++ Assets/Script/Graphic/WindowAlpha.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowAlpha : MonoBehaviour {
+
+    private MeshRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<MeshRenderer>();
+        rend.material.color = new Color(1f, 1f, 1f, 0f);
+        //Debug.Log(rend.material.color);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
