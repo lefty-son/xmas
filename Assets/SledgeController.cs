f28ddd7e751d427425702aa6001e076798e44c27
lefty-son <dangen.effy@gmail.com>
2017-12-15 19:27:59 +0900

    push

---
+++ Assets/SledgeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SledgeController : MonoBehaviour {
+    private Rigidbody rigid;
+    public float speed;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * Time.time, Space.Self );
+        if(Input.GetKey(KeyCode.A)){
+            transform.eulerAngles -= Vector3.up * Time.deltaTime * 50;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += Vector3.up * Time.deltaTime * 50;
+        }
+        Debug.Log(transform.position.z);
+
+
+        // ONE
+        // ONE
+       
+        if (transform.position.z > 150f)
+        {
+            StagePosition.instance.starting.SetActive(false);
+            StagePosition.instance.CurrentIndex = 0;
+        }
+        if (transform.position.z > 350f)
+        {
+            StagePosition.instance.CurrentIndex = 1;
+        }
+        if (transform.position.z > 550f)
+        {
+            StagePosition.instance.CurrentIndex = 2;
+        }
+        if (transform.position.z > 750f)
+        {
+            StagePosition.instance.CurrentIndex = 3;
+        }
+        if (transform.position.z > 950f)
+        {
+            StagePosition.instance.CurrentIndex = 4;
+        }
+        if (transform.position.z > 1150f)
+        {
+            StagePosition.instance.CurrentIndex = 5;
+        }
+        if (transform.position.z > 1350f)
+        {
+            StagePosition.instance.CurrentIndex = 6;
+        }
+        if (transform.position.z > 1550f)
+        {
+            StagePosition.instance.CurrentIndex = 7;
+        }
+    }
+}
---
+++ Assets/StagePosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StagePosition : MonoBehaviour {
+    public GameObject starting;
+    public GameObject[] stages;
+
+    public static StagePosition instance;
+
+    private int currentIndex;
+    public int CurrentIndex{
+        get{
+            return currentIndex;
+        }
+        set {
+            currentIndex = value;
+            MovePosition(currentIndex);
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    private void MovePosition(int _index){
+        stages[_index % 4].transform.localPosition = new Vector3(0f, 0f, 200 * (_index + 4));
+    }
+
+
+}
