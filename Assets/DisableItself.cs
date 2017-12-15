c720436e5a587a9c5298bdbd3e804ad69cfac4e4
lefty-son <dangen.effy@gmail.com>
2017-12-15 15:41:56 +0900

    racing start

---
+++ Assets/DisableItself.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableItself : MonoBehaviour {
+    public void Disable(){
+        gameObject.SetActive(false);
+    }
+
+}
