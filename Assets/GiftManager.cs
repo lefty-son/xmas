2902d667277acfe19efce37c977f1bde9f8594c7
lefty-son <dangen.effy@gmail.com>
2017-12-19 17:22:56 +0900

    added 15 gitfs

---
+++ Assets/GiftManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GiftManager : MonoBehaviour {
+
+    public static GiftManager instance;
+
+    public GameObject[] gifts, unlock, _name, price;
+    public Image[] holder;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    private void OnEnable()
+    {
+        CheckGiftTier();
+    }
+
+    public void CheckGiftTier(){
+        
+        Debug.Log("CHECKING TIER ...");
+        for (int i = 0; i <= PrefManager.instance.GetGiftTier() + 1; i++){
+            gifts[i].SetActive(true);
+        }
+
+        holder[PrefManager.instance.GetGiftTier() + 1].color = Color.black;
+        _name[PrefManager.instance.GetGiftTier() + 1].SetActive(false);
+        price[PrefManager.instance.GetGiftTier() + 1].SetActive(true);
+        unlock[PrefManager.instance.GetGiftTier() + 1].SetActive(true);
+        
+    }
+}
